ad3e8c61563867a2a1bf9ea680a59081fe16abe1
jerryjerryj <jerryjet@mail.ru>
2016-10-26 23:18:12 +0600

    делегаты

---
+++ Practice_4/Practice_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_4
+{
+    class Program
+    {
+        public static bool MyCheck(TransactionRequest request)
+        {
+            return true;
+        }
+        public static Transaction MyRegister(TransactionRequest request)
+        {
+            return new Transaction();
+        }
+        public static void MySave(Transaction request)
+        {
+        }
+        static void Main(string[] args)
+        {
+            var transactionProcessor= new TransactionProcessor(new Check(MyCheck), new Register(MyRegister), new Save(MySave));
+            transactionProcessor.Process(new TransactionRequest());
+        }
+    }
+}
---
+++ Practice_4/Practice_4/TransactionProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_4
+{
+    public delegate bool Check(TransactionRequest request);
+    public delegate Transaction Register(TransactionRequest request);
+    public delegate void Save(Transaction transaction);
+    public class Transaction { }
+    public class TransactionRequest { }
+    public class TransactionProcessor
+    {
+        Check check;
+        Register register;
+        Save save;
+
+        public TransactionProcessor( Check check,Register register,Save save)
+        {
+            this.check = check;
+            this.register = register;
+            this.save = save;
+        }
+        public Transaction Process(TransactionRequest request)
+        {
+            if (!check(request))
+                throw new ArgumentException();
+            var result = register(request);
+            save(result);
+            return result;
+        }
+    }
+}
